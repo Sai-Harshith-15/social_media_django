73f640b9f9d3e93798bc3032649a805de2f94729
Sai-Harshith-15 <saiharshith15.git@gmail.com>
2024-12-29 22:21:31 +0530

    added the app with urls and serializer
    added the app with urls and serializer

--- server/socialmedia/socialmedia/settings.py
+++ server/socialmedia/socialmedia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +51,18 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173/"
+]
+
+CORS_ALLOW_ALL_ORIGINS: True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(f'{BASE_DIR,} media')
 
 ROOT_URLCONF = 'socialmedia.urls'
 
